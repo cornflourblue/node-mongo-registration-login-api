5ae017efd75b98a8aa55dccf2a7d960734eefa6e
Jason Watmore <jason.watmore@hotmail.com>
2018-12-04 17:25:28 +1100

    made compatible with heroku

--- _helpers/db.js
+++ _helpers/db.js
@@ -1,6 +1,6 @@
 const config = require('config.json');
 const mongoose = require('mongoose');
-mongoose.connect(config.connectionString);
+mongoose.connect(process.env.MONGODB_URI || config.connectionString);
 mongoose.Promise = global.Promise;
 
 module.exports = {
--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use('/users', require('./users/users.controller'));
 app.use(errorHandler);
 
 // start server
-const port = process.env.NODE_ENV === 'production' ? 80 : 4000;
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
 const server = app.listen(port, function () {
     console.log('Server listening on port ' + port);
 });
