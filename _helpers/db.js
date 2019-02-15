78fdcc95a4fa1f7889e4e74b0bbbe5f412ae5b01
jwatmore <jason.watmore@hotmail.com>
2019-02-14 17:07:51 -0800

    Fixed: DeprecationWarning: collection.ensureIndex is deprecated. Use createIndexes instead

--- _helpers/db.js
+++ _helpers/db.js
@@ -1,6 +1,6 @@
 const config = require('config.json');
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI || config.connectionString, { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || config.connectionString, { useCreateIndex: true, useNewUrlParser: true });
 mongoose.Promise = global.Promise;
 
 module.exports = {
