e8c6d7ff1acb4ea2e4f05f75ca3949e432fa2b69
BasHud <eugesma@gmail.com>
2020-04-02 15:54:42 -0300

    Modify folder structure

--- config/passport.ts
+++
@@ -1,6 +0,0 @@
- 
-const config: any = {
-  JWT_SECRET: 'naapiauthenticationtopsecret'
-}
-
-export default config;
---
+++ src/config/env.config.ts
@@ -0,0 +1,5 @@
+export default {
+    API_URI_PREFIX: '/api',
+    JWT_SECRET: 'e18a33b0-9866-4867-800a-d6ffcd8f1cbd',
+    TOKEN_LIFETIME: 1
+};
