d433045a12a9039f5c11baee29713c1fe814abc5
paul <paul.18.sma@gmail.com>
2020-04-02 18:38:15 -0300

    removes dist directory

--- dist/src/config/env.config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    API_URI_PREFIX: '/api',
-    JWT_SECRET: 'e18a33b0-9866-4867-800a-d6ffcd8f1cbd',
-    TOKEN_LIFETIME: 1,
-    CONNECTION_STRING: 'mongodb://localhost/prescription-validator-api'
-};
--- dist/src/config/passport.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const config = {
-    JWT_SECRET: 'naapiauthenticationtopsecret'
-};
-exports.default = config;
