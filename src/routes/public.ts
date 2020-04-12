87cd7e6bf335dda2bf55bf5277b6c070cfbfdf56
paul <paul.18.sma@gmail.com>
2020-04-11 21:46:49 -0300

    adds public routes in separated file

---
+++ src/routes/public.ts
@@ -0,0 +1,16 @@
+import { Request, Response, Router } from 'express';
+
+class PublicRoutes{
+
+  constructor(private router: Router = Router()){}
+
+  // deefine your public routes inside of routes function
+  public routes(): Router{
+    // this.router.get('home', (req: Request, res: Response): Response => { return res.send('Welcome home') } ) // example
+
+    return this.router;
+  }
+}
+
+const publicRoutes: PublicRoutes = new PublicRoutes();
+export default publicRoutes.routes();
