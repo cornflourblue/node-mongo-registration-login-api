29609ec470400659051403196bf6e8c003bdc069
paul <paul.18.sma@gmail.com>
2020-04-13 20:35:04 -0300

    fix failed login message, and changes middleware function name

--- src/middlewares/passport-config.middleware.ts
+++ src/middlewares/passport-config.middleware.ts
@@ -54,7 +54,7 @@ passport.use(new LocalStrategy({
         if(!user){
             user = await User.findOne({ username: identifier });
             if(!user){
-                return done(null, false, {message: 'El usuario o contraseña que has ingresado es incorrecto. Por favor intenta denuevo.'});
+                return done(null, false, {message: 'El usuario o contraseña que has ingresado es incorrecto. Por favor intenta de nuevo.'});
             }
         }
 
@@ -63,7 +63,7 @@ passport.use(new LocalStrategy({
 
         // if not, handle it
         if(!isMatch){
-            return done(null, false, {message: 'El usuario o contraseña que has ingresado es incorrecto. Por favor intenta denuevo.'});
+            return done(null, false, {message: 'El usuario o contraseña que has ingresado es incorrecto. Por favor intenta de nuevo.'});
         }
         // otherwise, return the user
         done(null, user);
@@ -88,7 +88,7 @@ export const passportMiddlewareLocal = (req: Request, res: Response, next: NextF
     authenticationMiddleware(req, res, next, 'local');
 };
 
-export const passportMiddlewareJwt = (req: Request, res: Response, next: NextFunction) => {
+export const checkAuth = (req: Request, res: Response, next: NextFunction) => {
     passport.authenticate('jwt', {session: false}, (err, user, info) => {
 
         if (err) { return next(err) }
