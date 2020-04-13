21f4c2e0187f1071084941045dd32a3fd3873526
paul <paul.18.sma@gmail.com>
2020-04-13 20:41:51 -0300

    removes secretControll

--- src/routes/public.ts
+++ src/routes/public.ts
@@ -1,6 +1,4 @@
 import { Request, Response, Router } from 'express';
-import secretController from '../controllers/secret.controller';
-
 class PublicRoutes{
 
   constructor(private router: Router = Router()){}
@@ -8,8 +6,6 @@ class PublicRoutes{
   // deefine your public routes inside of routes function
   public routes(): Router{
     // this.router.get('home', (req: Request, res: Response): Response => { return res.send('Welcome home') } ) // example
-    this.router.post('/set-permissions-role', secretController.assignPermissionsToRole);
-    this.router.post('/create-permission', secretController.newPermission);
     return this.router;
   }
 }
