4e24dc8174aab4bd90b14af8f47f2b503cf28bec
paul <paul.18.sma@gmail.com>
2020-04-20 20:12:16 -0300

    improves: responses on token [expired, invalid, no token provided], and invalid credentials

--- src/middlewares/passport-config.middleware.ts
+++ src/middlewares/passport-config.middleware.ts
@@ -2,8 +2,9 @@ import {Request, Response, NextFunction} from 'express';
 import passport from 'passport';
 import passportJwt from 'passport-jwt';
 import passportLocal from 'passport-local';
-import config from '../config/env.config';
+import { env, httpCodes } from '../config/config';
 import User from '../models/user.model';
+import IUser from '../interfaces/user.interface';
 
 const JwtStrategy = passportJwt.Strategy;
 const LocalStrategy = passportLocal.Strategy;
@@ -16,24 +17,25 @@ const ExtractJwt = passportJwt.ExtractJwt;
 //  -user exists
 passport.use(new JwtStrategy({
     jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-    secretOrKey: (process.env.JWT_SECRET || config.JWT_SECRET)
-}, async (payload, done) => {
+    secretOrKey: (process.env.JWT_SECRET || env.JWT_SECRET)
+}, async (payload, done: (err?: any, user?: IUser | boolean, info?: {code: number, message: string}) => any | Response) => {
     try{
-        const expirationDate = new Date(payload.exp * 1000);
+        const expirationDate = new Date(payload.exp);
         if(expirationDate < new Date()) {
-            return done(null, false, {message: 'expired token'});
+            return done(null, false, {code: httpCodes.EXPIRED_TOKEN, message: 'El token ha expirado'});
         }
         // find the user specified in token
         const user = await User.findOne({ _id: payload.sub }).select('_id');
 
         // if user doesn't exists, handle it
         if(!user){
-            return done(null, false, {message: 'Unauthorized.'});
+            return done(null, false, {code: httpCodes.UNAUTHORIZED, message: 'Debe iniciar sesión'});
         }
 
         // otherwise, return the user
         done(null, user);
     }catch(err){
+        console.log('in error');
         done(err, false);
     }
 }));
@@ -45,7 +47,7 @@ passport.use(new JwtStrategy({
 passport.use(new LocalStrategy({
     usernameField: 'identifier',
     passwordField: 'password'
-}, async (identifier, password, done) => {
+}, async (identifier, password, done: (err?: any, user?: IUser | boolean, info?: {code: number, message: string}) => any | Response ) => {
     try{
         // find the user given the identifier
         let user = await User.findOne({ email: identifier });
@@ -54,7 +56,7 @@ passport.use(new LocalStrategy({
         if(!user){
             user = await User.findOne({ username: identifier });
             if(!user){
-                return done(null, false, {message: 'El usuario o contraseña que has ingresado es incorrecto. Por favor intenta de nuevo.'});
+                return done(null, false, {code: httpCodes.UNAUTHORIZED, message: 'El usuario o contraseña que has ingresado es incorrecto. Por favor intenta de nuevo.'});
             }
         }
 
@@ -63,7 +65,7 @@ passport.use(new LocalStrategy({
 
         // if not, handle it
         if(!isMatch){
-            return done(null, false, {message: 'El usuario o contraseña que has ingresado es incorrecto. Por favor intenta de nuevo.'});
+            return done(null, false, {code: httpCodes.UNAUTHORIZED, message: 'El usuario o contraseña que has ingresado es incorrecto. Por favor intenta de nuevo.'});
         }
         // otherwise, return the user
         done(null, user);
@@ -73,14 +75,22 @@ passport.use(new LocalStrategy({
 }));
 
 const authenticationMiddleware = (req: Request, res: Response, next: NextFunction, authenticationType: string) => {
-    passport.authenticate(authenticationType, {session: false}, (err, user, info) => {
-        if (err) { return next(err) }
-        if (!user) {
-            return res.status(401).json(info);
+    passport.authenticate(authenticationType, {session: false}, (err, user: IUser | boolean, info?: {code: number, message: string}): any | Response => {
+        try{
+
+            if (err) return next(err)
+
+            if(typeof(info) !== 'undefined') return res.status(info.code).json({message: info.message});
+
+            req.user = user;
+            next();
+        }catch(error){
+            if(error.code == 'ERR_HTTP_INVALID_STATUS_CODE') return res.status(httpCodes.UNAUTHORIZED).json({message: 'Debe iniciar sesión'});
+
+            return res.status(500).json('Server Error')
         }
-        req.user = user;
-        next();
     })(req, res, next);
+
 };
 
 // authentication methods
@@ -89,15 +99,5 @@ export const passportMiddlewareLocal = (req: Request, res: Response, next: NextF
 };
 
 export const checkAuth = (req: Request, res: Response, next: NextFunction) => {
-    passport.authenticate('jwt', {session: false}, (err, user, info) => {
-
-        if (err) { return next(err) }
-        else if(info instanceof Error){
-            return res.status(403).json({message: 'No Autorizado'});
-        } else if (!user) {
-            return res.status(401).json(info);
-        }
-        req.user = user;
-        next();
-    })(req, res, next);
+    authenticationMiddleware(req, res, next, 'jwt');
 }
