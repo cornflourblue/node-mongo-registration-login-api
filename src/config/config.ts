1cd420763a8ae1d22b99b8eb5a578a403d1ef97d
paul <paul.18.sma@gmail.com>
2020-04-20 20:06:40 -0300

    rename 'config.en.ts'v file to 'config.ts', adnd adds httpCodes map

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,12 @@
-export default {
+export const env = {
     API_URI_PREFIX: '/api',
     JWT_SECRET: 'e18a33b0-9866-4867-800a-d6ffcd8f1cbd',
     TOKEN_LIFETIME: 1,
     MONGODB_CONNECTION: 'mongodb://localhost/prescription-validator-api'
 };
+
+export const httpCodes = {
+    UNAUTHORIZED: 401,
+    FORBIDDEN: 403,
+    EXPIRED_TOKEN: 406,
+  };
