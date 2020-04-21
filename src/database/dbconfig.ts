6aac2c9604d4c18c42bb740673f11a3e306dabaf
paul <paul.18.sma@gmail.com>
2020-04-20 23:08:39 -0300

    fix: 'useFindAndModify' in 'true', the  and  are deprecated

--- src/database/dbconfig.ts
+++ src/database/dbconfig.ts
@@ -3,11 +3,12 @@ import { env } from '../config/config';
 
 export const initializeMongo = (): void => {
     const MONGO_URI = `${(process.env.MONGODB_URI || env.MONGODB_CONNECTION)}`;
-    mongoose.set('useFindAndModify', true);
+    // mongoose.set('useFindAndModify', true);
     // mongoose.set('debug', true);
     mongoose.connect(MONGO_URI, {
         useNewUrlParser: true,
         useCreateIndex: true,
-        useUnifiedTopology: true
+        useUnifiedTopology: true,
+        useFindAndModify: false
     }).then( db => console.log('DB is connected') );
 }
