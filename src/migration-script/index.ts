8155e7e6008e0422a1c591761521babd893fb37c
paul <paul.18.sma@gmail.com>
2020-05-08 14:10:29 -0300

    adapt the new v2 prescription migration

--- src/migration-script/index.ts
+++ src/migration-script/index.ts
@@ -3,15 +3,12 @@ import { env } from '../config/config';
 
 
 // interface
-import IPrescriptionOld from './interfaces/prescription-deprecated.interface';
-import IPrescription from '../interfaces/prescription.interface';
 import { PrescriptionClass }  from './classes/prescriptions.class';
-
-import User from '../models/user.model';
-
+import IPrescriptionOld from './interfaces/prescription-deprecated.interface'; //old prescriptions
 
 
 
+// init db connections
 const initializeMongo = (): void => {
     const MONGO_URI = `${(process.env.MONGODB_URI || env.MONGODB_CONNECTION)}`;
     mongoose.Promise = Promise;
@@ -22,47 +19,39 @@ const initializeMongo = (): void => {
         useFindAndModify: false
     }).then( mongoose => {
         console.log('DB is connected');
-        prescriptionMigration();
+        prescriptionMigration().then(() => {
+            mongoose.disconnect();
+        });
     });
 }
 
 async function prescriptionMigration(){
-    console.log(">> INICIANDO PROCESO...");
 
-
+    console.log(">> INICIANDO PROCESO DE ACTUALIZACIÓN...");
 
     const prescriptionClass = new PrescriptionClass();
 
     try{
 
-      const prescriptions: IPrescriptionOld[] = [];
-    //   const prescriptions: OldPrescription[] = await prescriptionClass.getPrescriptions();
+      const prescriptionsDeprecated: IPrescriptionOld[] = await prescriptionClass.getPrescriptions();
 
-      console.log(`>> CANTIDAD DE PRESCRIPCIONES QUE DEBEN SER ACTUALIZADAS: ${prescriptions.length}`);
+      console.log(`>> CANTIDAD DE PRESCRIPCIONES QUE DEBEN SER ACTUALIZADAS: ${prescriptionsDeprecated.length}`);
       console.log(`>> COMENZANDO ACTUALIZACION....`);
 
-        if(prescriptions){
-            // actualizamos los nombres de los atributos de las prescripciones
-            // await prescriptionClass.changeToDeprecatedField(prescriptions);
-
-            // luego buscamos las mismas prescripciones (ya con los atributos actualizados) y rellenamos los atributos con datos embebidos
-            // const prescriptions_ids: any = await Promise.all(prescriptions.map(function(pres) { return pres._id; }));
-            // const prescriptionsToUpdate: IPrescription[] = await prescriptionClass.getUpdatedPrescriptionsAttriubte(prescriptions_ids);
-// console.log(prescriptionsToUpdate[0].user_deprecated,'from index');
-            // await prescriptionClass.updateEmbeddedFields(prescriptionsToUpdate);
+        if(prescriptionsDeprecated){
+            await prescriptionClass.updateEmbeddedFields(prescriptionsDeprecated);
 
 
         }else{
             console.log(">> NO SE ENCONTRARON PRESCRIPCIONES PARA ACTUALIZAR")
         }
 
-        // console.log('>> FIN PROCESO =====================');
+        console.log('>> FIN PROCESO =====================');
 
     }catch(err){
         console.log("UN ERROR OCURRIO ");
         console.log(err);
     }
 }
-
 
 initializeMongo();
